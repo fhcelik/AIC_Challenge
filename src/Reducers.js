8497009aa2eb913a2772fc367dbec97dc801719c
Graham Pinhey <gpinhey@uwaterloo.ca>
2018-04-06 14:41:21 -0600

    Fixing reducer to not stomp units when updating value

--- src/Reducers.js
+++ src/Reducers.js
@@ -9,7 +9,10 @@ formulaCardsReducerMap[ActionTypes.changeFormulaArg] =
   (formulaCards, action) => {
     const cardId = action.payload.formulaCard;
     const newCard = Object.assign({}, formulaCards[cardId],
-      {argvals: Object.assign({}, formulaCards[cardId].argvals, action.payload.argvals)});
+      {argvals: Object.assign({}, formulaCards[cardId].argvals)});
+    for(const arg in newCard.argvals) {
+      Object.assign(newCard.argvals[arg], action.payload.argvals[arg]);
+    }
     return formulaCards.map((fc, index) => cardId == index ? newCard : fc);
   };
 
